351ba181e36a40eab4e3363d460e7ef4d9619618
Michael <mvhenderson.4@icloud.com>
2017-06-20 16:30:05 -0500

    add foreign key to customer model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,6 +11,8 @@ namespace Vidly.Models
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
 
     }
 }
