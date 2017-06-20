32253e5ecd86d1551c5a34a7f065fad64fb6da79
Michael <mvhenderson.4@icloud.com>
2017-06-19 20:47:36 -0500

    Fix Bootstrap file.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/yeti-bootstrap.min.css",
+                      "~/Content/yeti-bootstrap.css",
                       "~/Content/site.css"));
         }
     }
