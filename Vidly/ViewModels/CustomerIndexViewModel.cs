559bbc5aa4516065fb6c5fab9692ebc6da5595d7
Michael <mvhenderson.4@icloud.com>
2017-06-20 19:03:46 -0500

    View list of customers from customer controller and index action

---
+++ Vidly/ViewModels/CustomerIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerIndexViewModel
+    {
+        public IEnumerable<Customer> Customer { get; set; }
+    }
+}
