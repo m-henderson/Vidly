a9498833d74edaa9fca2675db24397efe7ca12c8
Michael <mvhenderson.4@icloud.com>
2017-06-25 08:53:11 -0500

    Add Validation Messages

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -6,13 +6,19 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
         public Genre Genres { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
         public DateTime? DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
+
+        [Required]
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
