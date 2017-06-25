13d2619d7e78ebc38a6d899e98f66669e5fb1570
Michael <mvhenderson.4@icloud.com>
2017-06-25 10:57:08 -0500

    fix pure view model

--- Vidly/ViewModels/NewMovieViewModel.cs
+++ Vidly/ViewModels/NewMovieViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -8,8 +9,34 @@ namespace Vidly.ViewModels
 {
     public class NewMovieViewModel
     {
-        public Movie Movie { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte? GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        [_1To20NumberOfMoviesInStock]
+        public byte? NumberInStock { get; set; }
+        public byte NumberAvailable { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit Movie" : "New Movie";
+            }
+        }
 
     }
 }
