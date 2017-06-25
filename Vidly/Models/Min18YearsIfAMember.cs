50cd0e47b7daf6ae3613912f13dccac671f81b31
Michael <mvhenderson.4@icloud.com>
2017-06-24 22:16:43 -0500

    Add customer validation requirements.

--- Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -10,7 +10,21 @@ namespace Vidly.Models
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return base.IsValid(value, validationContext);
+            var customer = (Customer) validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthday is required. ");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("Customer should be at least 18 years of age or older to go on a membership.");
+               
+            
         }
     }
 }
