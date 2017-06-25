a31230f7878e15b8979afc7db56afbcb0de0b85b
Michael <mvhenderson.4@icloud.com>
2017-06-24 22:00:55 -0500

    Create class for customer validation attributes

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+    }
+}
